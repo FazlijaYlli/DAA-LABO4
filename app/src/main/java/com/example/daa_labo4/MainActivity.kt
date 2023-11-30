1de671fe0c5f1e8f7de15817694820cb6ef5f93e
Ylli Fazlija <fazlijaylli@gmail.com>
2023-11-30 11:29:17 +0100

    Added action bar menus to the main activity

--- app/src/main/java/com/example/daa_labo4/MainActivity.kt
+++ app/src/main/java/com/example/daa_labo4/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.daa_labo4
 import com.example.daa_labo4.models.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.actions, menu)
+        return true
     }
 }
