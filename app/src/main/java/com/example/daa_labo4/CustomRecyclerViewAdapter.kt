6c6613697f27c438d23a3aa6d1f0d496bfefa839
PrazTobie <tobie.praz@heig-vd.ch>
2023-11-30 14:07:26 +0100

    WIP RecyclerView

--- app/src/main/java/com/example/daa_labo4/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/daa_labo4/CustomRecyclerViewAdapter.kt
@@ -1,12 +1,20 @@
 package com.example.daa_labo4
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.Group
 import androidx.recyclerview.widget.RecyclerView
+import com.example.daa_labo4.models.NoteAndSchedule
+import com.example.daa_labo4.models.State
+import com.example.daa_labo4.models.Type
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
-class CustomRecyclerViewAdapter(private val dataSet: Array<String>) :
+class CustomRecyclerViewAdapter(private val dataSet: Array<NoteAndSchedule>) :
     RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder>() {
 
     /**
@@ -14,11 +22,48 @@ class CustomRecyclerViewAdapter(private val dataSet: Array<String>) :
      * (custom ViewHolder)
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
+        val title: TextView
+        val desc: TextView
+        val icon: ImageView
+        val status: ImageView
+        val time: TextView
+        val scheduleGroup: Group
 
         init {
             // Define click listener for the ViewHolder's View
-            textView = view.findViewById(R.id.textView)
+            title = view.findViewById(R.id.noteTitle)
+            desc = view.findViewById(R.id.noteDescription)
+            icon = view.findViewById(R.id.noteType)
+            status = view.findViewById(R.id.noteStatus)
+            time = view.findViewById(R.id.noteTime)
+            scheduleGroup = view.findViewById(R.id.noteSchedule)
+        }
+
+        fun bind(note : NoteAndSchedule) {
+            title.text = note.note.title
+            desc.text = note.note.text
+            icon.setImageResource(when(note.note.type) {
+                Type.NONE -> R.drawable.note
+                Type.FAMILY -> R.drawable.family
+                Type.SHOPPING -> R.drawable.shopping
+                Type.TODO -> R.drawable.todo
+                Type.WORK -> R.drawable.work
+            })
+
+            val color = when(note.note.state) {
+                State.DONE -> Color.GREEN
+                State.IN_PROGRESS -> Color.GRAY
+            }
+            icon.setColorFilter(color)
+
+            if (note.schedule != null) {
+                scheduleGroup.visibility = View.VISIBLE
+                val color = if(note.schedule.date > Calendar.getInstance()) Color.RED else Color.GRAY
+                status.setColorFilter(color)
+                time.text = SimpleDateFormat.getInstance().format(note.schedule.date.time)
+            }  else {
+                scheduleGroup.visibility = View.GONE
+            }
         }
     }
 
@@ -36,10 +81,14 @@ class CustomRecyclerViewAdapter(private val dataSet: Array<String>) :
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.textView.text = dataSet[position]
+        viewHolder.bind(dataSet[position])
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
+
+    override fun getItemViewType(position: Int): Int {
+        return dataSet[position].note.type.ordinal
+    }
 
 }
